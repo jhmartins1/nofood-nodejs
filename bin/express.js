aea9a9f90f9c5c919cfad7eb3fd805fbee5ea2cf
jhmartins1 <jhmartinsrdo@gmail.com>
2021-08-31 02:29:39 -0300

    primeiro commit

---
+++ bin/express.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const variables = require('../bin/configuration/variables');
+
+//routers
+const categoriaRouter = require('../routes/catergoria-router');
+const produtoRouter = require('../routes/produto-router');
+const usuarioRouter = require('../routes/usuario-router');
+
+//Criando/Invocando a Api/Server Web do Express
+const app = express();
+
+//Configuração de parse do JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Configurando a conexão com banco de dados
+mongoose.connect(variables.Database.connection,{useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+//Configurando as rotas
+app.use('/api/categoria', categoriaRouter);
+app.use('/api/produto', produtoRouter);
+app.use('/api/usuario', usuarioRouter);
+
+
+//Exportando nossa Api
+module.exports = app;
+
+
+// Api -> MIDDLEWARES -> Rotas -> Controller -> Repository -> Banco
---
+++ controllers/produto-controller.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const repository = require('../repositories/produto-repository');
+const ctrlBase = require('../bin/base/controller-base');
+const validation = require('../bin/helpers/validation');
+const _repo = new repository();
+
+function produtoController() {
+
+}
+
+produtoController.prototype.post = async (req, res) => {
+    let _validationContract = new validation();
+    _validationContract.isRequired(req.body.nome, 'O nome do produto é obrigatorio');
+    _validationContract.isRequired(req.body.descricao, 'A descrição do produto é obrigatoria');
+    _validationContract.isRequired(req.body.foto, 'A foto do produto é obrigatoria');
+    _validationContract.isRequired(req.body.preco, 'O preço do produto é obrigatorio');
+
+    if (req.body.preco)
+        _validationContract.isTrue(req.body.preco == 0, 'O preço do produto deve ser maior que Zero.');
+
+    ctrlBase.post(_repo, _validationContract, req, res);
+};
+
+produtoController.prototype.put = async (req, res) => {
+    let _validationContract = new validation();
+
+    _validationContract.isRequired(req.body.nome, 'O nome do produto é obrigatorio');
+    _validationContract.isRequired(req.body.descricao, 'A descrição do produto é obrigatoria');
+    _validationContract.isRequired(req.body.foto, 'A foto do produto é obrigatoria');
+    _validationContract.isRequired(req.body.preco, 'O preço do produto é obrigatorio');
+
+    if (req.body.preco)
+        _validationContract.isTrue(req.body.preco == 0, 'O preço do produto deve ser maior que Zero.');
+
+    ctrlBase.put(_repo, _validationContract, req, res);
+};
+
+produtoController.prototype.get = async (req, res) => {
+    ctrlBase.get(_repo, req, res);
+};
+
+produtoController.prototype.getById = async (req, res) => {
+    ctrlBase.getById(_repo, req, res);
+};
+
+produtoController.prototype.delete = async (req, res) => {
+    ctrlBase.delete(_repo, req, res);
+};
+
+module.exports = produtoController;
